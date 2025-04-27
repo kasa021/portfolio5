26ce117e51879dbee4521c5d465605e22f08c161
kasa021 <mkafa.k11@gmail.com>
2025-04-27 10:55:09 +0900

    KVを使用しないので修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,9 @@ export default defineConfig({
   adapter: cloudflare({
     platformProxy: {
       enabled: true
-    }
-  })
+    },
+    imageService: {
+      driver: "cloudflare",
+    },
+  }),
 });
